a1a29da2e0e6cef2d743852df067651e2709c380
CCulver831 <c.culver831@gmail.com>
2020-05-11 18:44:23 -0700

    completed Bit-masking Example. Moving to Bitshifting and Bitboards soon :)

--- Programming Etiquette/Assets/AttributeManager.cs
+++ Programming Etiquette/Assets/AttributeManager.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+//This Class shows examples of usinf bitflags to represent player stats
 public class AttributeManager : MonoBehaviour
 {
     static public int MAGIC = 16;
     public Text attributeDisplay;
-
-    int attirbutes = 0;
+    static public int FLY = 8;
+    static public int INTELLIGENCE = 4;
+    public int attributes = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +19,33 @@ public class AttributeManager : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Magic")
-        attirbutes |= MAGIC;
+        {
+            attributes |= MAGIC;
+        }
+        else if (other.gameObject.tag == "Fly")
+        {
+            attributes |= FLY;
+        }
+       else if (other.gameObject.tag == "Intelligence")
+        {
+            attributes |= INTELLIGENCE;
+        }
+        //Note: To remove an attribute
+        /*
+         *      else if (other.gameObject.tag == "UNDO")
+        {
+            attirbutes &= ~INTELLIGENCE;
+        }
+        */
+
+
     }
     // Update is called once per frame
     void Update()
     {
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
         attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0); //keeps text in center of screen facing player
-        attributeDisplay.text = Convert.ToString(attirbutes, 2).PadLeft(8, '0');
+        attributeDisplay.text = Convert.ToString(attributes, 2).PadLeft(8, '0');
     }
        
 }
---
+++ Programming Etiquette/Assets/DoorManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This script displays bit masking to grant access to a dorr with specific requirements.
+public class DoorManager : MonoBehaviour
+{
+    int doorType = AttributeManager.MAGIC;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if((collision.gameObject.GetComponent<AttributeManager>().attributes & doorType) != 0) //if the bits match, we have the attribute we're looking for
+        {
+            GetComponent<BoxCollider>().isTrigger = true;
+            //Make door solid again
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<BoxCollider>().isTrigger = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
